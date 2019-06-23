d92953f382fbbc890277fac376d1be9520d1fc05
Rahul Kumar Singh <51693379+rahul-001@users.noreply.github.com>
2019-06-23 13:24:10 +0530

    Add files via upload
    Create Nodejs Server with mongodb connectivity

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.listen(4000, () => console.log('Server Started'))
