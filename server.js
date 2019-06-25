c7a40a01f41cbad13f8da954f97aa3be195fe3b1
rahul-001 <singhrahulkumar183@gmail.com>
2019-06-25 12:56:19 +0530

    Create Nodejs Server with mongodb connectivity

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-app.listen(4000, () => console.log('Server Started'))
+app.listen(3000, () => console.log('Server Started'))
